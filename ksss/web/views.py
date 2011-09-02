60c4b7809cf41125d98bcc3803b2110ea48ddb88
Carl Littke <carl.littke@gmail.com>
2011-09-02 16:29:58 +0200

    edit_boat and damages now autofills the forms (when you want to edit something) using instance instead of hardcoded initials

--- ksss/web/views.py
+++ ksss/web/views.py
@@ -34,9 +34,9 @@ def add_boat(request):
     }, context_instance=RequestContext(request))
 
 def edit_boat(request, boat_id):
+    to_edit = models.Boat.objects.get(id=boat_id)
     if request.method == 'POST':
-        edit_id = models.Boat.objects.get(id=boat_id)
-        form = EditBoat(request.POST, instance = edit_id)
+        form = EditBoat(request.POST, instance = to_edit)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/new_damage/thanks/')
@@ -45,19 +45,9 @@ def edit_boat(request, boat_id):
             boat_id = int(boat_id)
         except ValueError:
             raise Http404()
-        form = EditBoat(
-            initial = {
-            'boat_type': models.Boat.objects.get(id=boat_id).boat_type,
-            'name': models.Boat.objects.get(id=boat_id).name,
-            'motor': models.Boat.objects.get(id=boat_id).motor,
-            'motor_hp': models.Boat.objects.get(id=boat_id).motor_hp,
-            'bought': models.Boat.objects.get(id=boat_id).bought,
-            'service': models.Boat.objects.get(id=boat_id).service,
-            'location': models.Boat.objects.get(id=boat_id).location
-        })
+        form = EditBoat(instance = to_edit)
         
     return render_to_response('edit.html', {
-        'Id': models.Boat.objects.get(id=boat_id),
         'form': form
     }, context_instance=RequestContext(request))
 
@@ -70,27 +60,22 @@ def delete(request, id, item):
 def damage(request, dmg_id=None):
     if request.method == 'POST':
         if dmg_id:
-            edit_dmg_id = models.ReportedDamage.objects.get(id=dmg_id)
-            form = AddDamage(request.POST, instance = edit_dmg_id)
+            to_edit = models.ReportedDamage.objects.get(id=dmg_id)
+            form = AddDamage(request.POST, instance = to_edit)
         else:
             form = AddDamage(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/new_damage/thanks/')
+
     elif dmg_id:
         try:
             dmg_id = int(dmg_id)
         except ValueError:
             raise Http404()
+        to_edit = models.ReportedDamage.objects.get(id=dmg_id)
+        form = AddDamage(instance = to_edit)
 
-        form = AddDamage(
-        initial = {
-            'boat': models.ReportedDamage.objects.get(id=dmg_id).boat,
-            'damage': models.ReportedDamage.objects.get(id=dmg_id).damage,
-            'description': models.ReportedDamage.objects.get(id=dmg_id).description,
-            'actions_taken': models.ReportedDamage.objects.get(id=dmg_id).actions_taken,
-            'actions_needed': models.ReportedDamage.objects.get(id=dmg_id).actions_needed
-        })
     else:
         form = AddDamage()
     return render_to_response('add.html', {
